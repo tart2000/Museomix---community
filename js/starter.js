d0e96c9aed2eab9c904e9b06aa93595d929b3acd
tart2000 <tart2000design@gmail.com>
2014-02-04 12:21:38 -0500

    HP finished + sweet random on Hub page

--- js/starter.js
+++ js/starter.js
@@ -14,7 +14,29 @@ $('.grab').hover(function() {
 	$(this).find('.unlock-btn').toggleClass('hide');
 });
 
+
 $('img').tooltip();
 
+
+$('#hub-tab').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+function randomNumber (top) {
+    var a = Math.floor(Math.random() * top);
+    return a;
+}
+
+function discussion (a) {
+	var discussionX = "<div class='bb-list'>";
+	for (var i = 0; i <= a ; i++) {
+		discussionX += "<div class='item-bb'><div class='cat-item-title'><div class='right people'>"+ randomNumber(20)+" <span class='glyphicon glyphicon-user'></span> <span class='badge'>"+ randomNumber(20)+"</span> "+ (i)+" minutes ago</div><a href='#'><h3><span class='glyphicon glyphicon-star-empty'></span> "+loremIpsum(7)+"</h3></a></div></div>";
+	}
+	discussionX += "</div>"
+	document.getElementById('discussion').innerHTML = discussionX;
+};
+
+discussion(10);
 
 });
